891cf1f234fab949f0fcebdd5ec7f30673f4633e
Jason Louie <jason.louie@berkeley.edu>
2024-08-20 16:27:54 -0700

    Updated the today view to query all tasks published within the last 24 hours.

--- todosite/daily/models.py
+++ todosite/daily/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.task_text
--- todosite/daily/views.py
+++ todosite/daily/views.py
@@ -1,6 +1,14 @@
+import datetime
+from .models import Task
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 def today(request):
-    return HttpResponse("This is the daily tasks page.")
+    last_24_hours = timezone.now() - datetime.timedelta(hours=24)
+    tasks = Task.objects.filter(pub_date__gte=last_24_hours)
+    context = {"latest_tasks": tasks}
+    # return render(request, "daily/today.html", context)
+    return HttpResponse("This page will be filled with a HTML template soon.")
