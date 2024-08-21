c24d4a83969b9a08e2efceb6d6d1066c1706b0b5
Jason Louie <jason.louie@berkeley.edu>
2024-08-20 17:17:11 -0700

    Added basic style and created a basic HTML template. Displays the tasks created within the last day. Need to create tests and introduce more functionality.

--- todosite/daily/views.py
+++ todosite/daily/views.py
@@ -10,5 +10,5 @@ def today(request):
     last_24_hours = timezone.now() - datetime.timedelta(hours=24)
     tasks = Task.objects.filter(pub_date__gte=last_24_hours)
     context = {"latest_tasks": tasks}
-    # return render(request, "daily/today.html", context)
-    return HttpResponse("This page will be filled with a HTML template soon.")
+    return render(request, "daily/today.html", context)
+
